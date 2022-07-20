58e737c9ca1c697d3d65db9303c5e00da1ae7211
RudyStark <89338357+RudyStark@users.noreply.github.com>
2022-07-20 14:51:52 +0200

    Adding Security + Setting up login form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,11 +3,13 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
 use App\Entity\Recipe;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -51,6 +53,19 @@ class AppFixtures extends Fixture
             }
 
             $manager->persist($recipe);
+        }
+        $manager->flush();
+
+        // Users
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setFullName($this->faker->name)
+                ->setPseudo(mt_rand(0, 1) == 1 ? $this->faker->userName : null)
+                ->setEmail($this->faker->email)
+                ->setRoles(['ROLE_USER'])
+                ->setPlainPassword('password');
+
+            $manager->persist($user);
         }
         $manager->flush();
     }
