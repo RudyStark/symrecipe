86330c3cdf2d8fb4abd51ecf2a17a743e65d2f43
RudyStark <89338357+RudyStark@users.noreply.github.com>
2022-07-26 15:10:53 +0200

    :sparkles: Update MailService with new class MailService- Improvment coding

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,30 +4,20 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Service\MailService;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
-
-
-    /**
-     * Summary of index
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @param MailerInterface $mailer
-     * @return Response
-     */
     #[Route('/contact', name: 'contact.index')]
     public function index(
         Request $request,
         EntityManagerInterface $manager,
-        MailerInterface $mailer
+        MailService $mailService
     ): Response {
         $contact = new Contact();
 
@@ -45,18 +35,15 @@ class ContactController extends AbstractController
             $manager->persist($contact);
             $manager->flush();
 
-            // Envoi d'un mail à l'administrateur
-            $email = (new TemplatedEmail())
-                ->from($contact->getEmail())
-                ->to('admin@symrecipe.fr')
-                ->subject($contact->getSubject())
-                ->html($contact->getMessage())
-                ->htmlTemplate('emails/contact.html.twig')
-                ->context([
-                    'contact' => $contact,
-                ]);
-
-            $mailer->send($email);
+            //Email
+            $mailService->sendEmail(
+                $contact->getEmail(),
+                $contact->getSubject(),
+                'emails/contact.html.twig',
+                [
+                    'contact' => $contact
+                ]
+            );
 
             $this->addFlash('success', 'Votre message a bien été envoyé.');
             return $this->redirectToRoute('contact.index');
---
+++ src/Service/MailService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+class MailService
+{
+
+    /**
+     * Summary of 
+     * @var MailerInterface
+     */
+    private MailerInterface $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    public function sendEmail(
+        string $from,
+        string $subject,
+        string $htmlTemplate,
+        array $context,
+        string $to = 'admin@symrecipe.fr',
+
+    ): void {
+        // Envoi d'un mail à l'administrateur
+        $email = (new TemplatedEmail())
+            ->from($from)
+            ->to($to)
+            ->subject($subject)
+            ->htmlTemplate($htmlTemplate)
+            ->context($context);
+
+        $this->mailer->send($email);
+    }
+}
